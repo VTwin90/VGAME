8fa393f194e6868d66bc75020ba644e8f1211193
VTwin90 <vanja.torp1@gmail.com>
2021-10-18 17:06:50 +0000

    add app route for home page

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import (
-     Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
@@ -17,12 +17,13 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+# Home page:
 @app.route("/")
-
+@app.route("/home")
+def home():
+    return render_template("base.html")
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
-
+    app.run(host = os.environ.get("IP"),
+            port = int(os.environ.get("PORT")),
+            debug = True)
