4aa246b7d7fd51a50ff183af045b59a11d75c12e
VTwin90 <vanja.torp1@gmail.com>
2021-11-07 00:45:00 +0000

    add 500 error functionality and template

--- app.py
+++ app.py
@@ -215,7 +215,12 @@ def delete(game_id):
 # 404 Errorhandler:
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html', error=error), 404
+    return render_template("404.html", error=error), 404
+
+# 500 Errorhandler:
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html", error=error), 500
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
