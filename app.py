a18d87c9118adc2fa027cd687dade8b8ef062475
VTwin90 <vanja.torp1@gmail.com>
2021-11-07 00:41:24 +0000

    add 404 error functionality app.py

--- app.py
+++ app.py
@@ -212,6 +212,10 @@ def delete(game_id):
         {"username": session["user"]})["username"]
     return redirect(url_for("profile", username=username))
 
+# 404 Errorhandler:
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', error=error), 404
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
